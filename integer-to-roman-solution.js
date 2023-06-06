ef8faa4c49048f87a6427681dd9b94b5d2431502
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-06-06 22:50:10 +0530

    solution ipload

---
+++ integer-to-roman-solution.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var intToRoman = function (num) {
+  var symbols = [
+    "M",
+    "CM",
+    "D",
+    "CD",
+    "C",
+    "XC",
+    "L",
+    "XL",
+    "X",
+    "IX",
+    "V",
+    "IV",
+    "I",
+  ];
+  var values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+
+  var roman = "";
+  var i = 0;
+  return roman;
+};
